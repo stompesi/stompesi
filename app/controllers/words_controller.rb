7aa7994a595f10c0c427fb204571694547b155c6
stompesi <stompesi@gmail.com>
2015-12-13 23:26:23 +0900

    word is synced with stompesi server.

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,7 @@
 class WordsController < ApplicationController
   #Word.update_all('remaining_dates = remaining_dates -1')
+  skip_before_filter :verify_authenticity_token
+
   def self.day
     [0, 1, 2, 4, 8, 16, 32, 64]
   end
@@ -18,25 +20,65 @@ class WordsController < ApplicationController
 
   def show_overlap
     words = Word.select(:word).distinct
-
     words.each do |word| 
-      unless Word.where(word: word.word).group('meaning').count('id').length == 1
-        @overlap_word_informaitons = Word.where(word: word.word)
-        break
+      result = get_stompesi_word(word)
+      unless result.length == 0
+        current_word = Word.where(word: word.word)
+        unless current_word[0]['meaning'] == result[0]['meaning']
+          current_word << result[0]
+          @overlap_word_informaitons = current_word
+          break
+        end
       end
-    end    
+    end  
+  end
+
+  def get_stompesi_word(word)
+    require 'net/http'
+    url = 'http://stompesi.herokuapp.com/words/get_word_info?word=' + word.word
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    JSON.parse(response)
+  end
+
+  def set_stompesi_word(input_word)
+    require 'uri'
+    require 'net/http'
+    require 'net/https'
+
+    uri = URI.parse('http://stompesi.herokuapp.com/words/overlap')
+    https = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
+
+    word = {}
+    word[:word] = input_word.word
+    word[:meaning] = input_word.meaning
+    word[:sentence] = input_word.sentence
+    word[:sentence_meaning] = input_word.sentence_meaning
+    word[:is_outside] = true
+
+    req.body = {word: word}.to_json
+    res = https.request(req)
   end
 
   def update_overlap
     word_informations = Word.where(word: params[:word][:word])
-
+    
     word_informations.each do |word|
-      word.update(word_params)
+      word.meaning = params[:word][:meaning]
+      word.sentence = params[:word][:sentence]
+      word.sentence_meaning = params[:word][:sentence_meaning]
+      word.save!
     end
 
-    redirect_to overlap_words_path
+    if params[:word][:is_outside].nil?
+      word = Word.find_by_word(params[:word][:word])
+      set_stompesi_word(word)
+      redirect_to overlap_words_path
+    else 
+      render json: {status: 200}
+    end
   end
-
 
 
   def memorize_all
@@ -61,8 +103,16 @@ class WordsController < ApplicationController
 
   def update
     vocabulary = Vocabulary.find(params[:word][:vocabulary_id])
-    word = Word.find(params[:id])
-    word.update(word_params)
+    update_word = Word.find(params[:id])
+
+    word_informations = Word.where(word: update_word.word)
+
+    word_informations.each do |word|
+      word.update(word_params)
+    end
+
+    update_word = Word.find(params[:id])
+    set_stompesi_word(update_word)
 
     redirect_to vocabulary_path(vocabulary)
   end
@@ -81,20 +131,26 @@ class WordsController < ApplicationController
   def create
     word = Word.find_by_word(params[:word][:word])
     vocabulary = Vocabulary.find(params[:word][:vocabulary_id])
-    if word.nil? 
-      
-      word = vocabulary.words.new(word_params)
-      word.remaining_dates = Time.now
-      word.save!
-    else
-      word.update(word_params)
+    unless word.nil? 
+      word_informations = Word.where(word: params[:word][:word])
+
+      word_informations.each do |word|
+        word.update(word_params)
+      end
     end
+
+    word = vocabulary.words.new(word_params)
+    word.remaining_dates = Time.now
+    word.save!
+
+    word = Word.find_by_word(params[:word][:word])
+    set_stompesi_word(word)
+
     redirect_to new_word_path(vocabulary_id: vocabulary.id)
   end
 
   def input_word 
     word = Word.find_by_word(params[:word])
-
     if word.nil? 
       render json: {isHaveWord: false}
     else

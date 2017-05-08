60b3d46f3bcc39b9c0bcb401514cc0af1f46dc59
stompesi <stompesi@gmail.com>
2017-05-08 14:57:20 +0900

    login function remove.

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
-Rails.application.routes.draw do
-  root 'home#index'
+# Rails.application.routes.draw do
+#   root 'home#index'
 
-  resources :vocabularies
-  resources :folders
+#   resources :vocabularies
+#   resources :folders
 
-  resources :words do
-    collection do
-      put :mutipule_update
-      patch :mutipule_update
-      get :get_word_from_other_information, to: 'words#get_word_from_other_information'
-      post :set_word_to_other_server, to: 'words#set_word_from_other_server'
-      get :input_word
-      get :memorize_all
-      get :memorize
-    end
-  end
+#   resources :words do
+#     collection do
+#       put :mutipule_update
+#       patch :mutipule_update
+#       get :get_word_from_other_information, to: 'words#get_word_from_other_information'
+#       post :set_word_to_other_server, to: 'words#set_word_from_other_server'
+#       get :input_word
+#       get :memorize_all
+#       get :memorize
+#     end
+#   end
 
-  get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
-  get 'signout', to: 'sessions#destroy'
+#   get 'auth/:provider/callback', to: 'sessions#create'
+#   get 'auth/failure', to: redirect('/')
+#   get 'signout', to: 'sessions#destroy'
 
-end
+# end
